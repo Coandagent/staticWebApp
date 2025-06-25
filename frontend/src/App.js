bbbb582b3e14c2e59f022ed7a06032798828a874
Bstad1991 <bstad91@outlook.dk>
2025-06-25 10:03:21 +0200

    DRAGAABLE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@
 // ──────────────────────────────────────────────────────────────────────────────
 
 import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   FaUserPlus,
